73964edb51cc518394ccc0a92e3e6c943f0bd25a
Mark Condello <mark@dcode.com.au>
2021-11-21 18:57:22 +1100

    fonts, buttons, colours

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,6 +1,19 @@
 <template>
-<button class="button">Generic button</button>
-  <img alt="Vue logo" src="../images/logos/vue.png">
+  <header>
+    <nav>
+      <h1>
+  <!-- <img alt="Vue logo" src="../images/logos/vue.png"> -->
+      </h1>
+      <ul>
+        <li><button>Work</button></li>
+        <li><button>About</button></li>
+        <li><button class="button -hollow sml">Contact</button></li>
+      </ul>
+    </nav>
+  </header>
+  <section class="intro bg-primary">
+    <button class="button">Generic button</button>
+  </section>
  </template>
 
 <script>
