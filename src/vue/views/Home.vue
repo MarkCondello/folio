b4650d4ecd17da4d847a56d722f49c1ed82a95dd
Mark Condello <mark@dcode.com.au>
2021-11-30 20:09:29 +1100

    -refines to the frame scroll settings

--- src/vue/views/Home.vue
+++ src/vue/views/Home.vue
@@ -17,10 +17,27 @@
   </frame>
   <frame
     bg-class="bg-primary"
+    bg-img-src="https://media.istockphoto.com/photos/white-truck-in-motion-uk-street-picture-id1284419710?b=1&k=20&m=1284419710&s=170667a&w=0&h=i8HrBjT5uYqG1tvQnAFENs__ZmGQl3eD_TZPPO-HulA="
+    intro-part-one="Best Rated Transport"
+    intro-part-two="Laravel, jQuery, Vue, Foundation, SCSS"
+    scroll-message="Work 01. Best Rated Transport"
+  >
+    <template v-slot:article>
+      <h1>Quote generating system</h1>
+      <div class="flex-cols">
+        <h2 class="lg-6 h3">A custom account based application that supports customer/company accounts, & a client admin.</h2>
+      </div>
+      <div class="flex-cols">
+        <a href="#" class="lg-3 button">view project</a>
+      </div>
+    </template>
+  </frame>
+  <frame
+    bg-class="bg-primary"
     bg-img-src="https://images.unsplash.com/photo-1449157291145-7efd050a4d0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZyZWUlMjBwaWMlMjBjaXR5JTIwYml1bGRpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
     intro-part-one="Australia Market Entry"
-    intro-part-two=" Laravel, Vue, SCSS"
-    scroll-message="Work 01. Best Rated Transport"
+    intro-part-two="Laravel, Vue, SCSS"
+    scroll-message="Work 02. Asia Market Entry"
   >
     <template v-slot:article>
       <h1>Business lead-building CMS</h1>
@@ -31,8 +48,6 @@
         <a href="#" class="lg-3 button">view project</a>
       </div>
     </template>
-    <template v-slot:plug>
-    </template>
   </frame>
 </template>
 <script>
@@ -41,3 +56,4 @@ export default {
   components: { Frame }
 }
 </script>
+// https://media.istockphoto.com/photos/white-truck-in-motion-uk-street-picture-id1284419710?b=1&k=20&m=1284419710&s=170667a&w=0&h=i8HrBjT5uYqG1tvQnAFENs__ZmGQl3eD_TZPPO-HulA=
