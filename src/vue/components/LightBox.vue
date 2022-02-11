85a138f5bd01d5927d1c8c15f7b7a03118318b54
Mark Condello <mark@dcode.com.au>
2022-02-11 22:35:22 +1100

    -refines to light box

--- src/vue/components/LightBox.vue
+++ src/vue/components/LightBox.vue
@@ -6,15 +6,21 @@
     >
       <div class="light-box">
         <button class="close" @click="closeLightBox">X</button>
-        <nav>
-          <button class="prev" @click="prevSlide"> prev </button>
-          <button class="next" @click="nextSlide"> next </button>
-        </nav>
+        <figure>
+          <!-- convert to inlie styles and transition the width height vals -->
+          <img :src="slides[activeSlideIndex].url"
+          :width="slides[activeSlideIndex].width"
+          :height="slides[activeSlideIndex].height" />
+        </figure>
         <footer>
-          <p v-html="slides[0].title" />
+          <p v-html="slides[activeSlideIndex].title" />
           <small>Image {{ activeSlideIndex + 1 }} of {{ slides.length }}</small>
         </footer>
       </div>
+      <nav>
+        <button class="prev" @click="prevSlide"> prev </button>
+        <button class="next" @click="nextSlide"> next </button>
+      </nav>
   </div>
 </template>
 
@@ -35,7 +41,21 @@ export default {
     slides: state => state.lightBoxStore.slides
   }),
   methods: {
-    ...mapActions(['closeLightBox'])
+    ...mapActions(['closeLightBox']),
+    prevSlide () {
+      if (this.activeSlideIndex === 0) {
+        this.activeSlideIndex = this.slides.length - 1
+      } else {
+        this.activeSlideIndex -= 1
+      }
+    },
+    nextSlide () {
+      if (this.activeSlideIndex === this.slides.length - 1) {
+        this.activeSlideIndex = 0
+      } else {
+        this.activeSlideIndex += 1
+      }
+    }
   }
 }
 </script>
