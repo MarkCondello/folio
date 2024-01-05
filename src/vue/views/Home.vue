6913d408f8a5d675815912ad23bcde440ab128eb
Mark <mark.condello@sparkgreen.com.au>
2024-01-05 12:53:40 +1100

    Update resume, change project order to asc

--- src/vue/views/Home.vue
+++ src/vue/views/Home.vue
@@ -8,7 +8,7 @@
     anchor-id="intro"
   >
     <template v-slot:article>
-      <h1>A Melbourne based FE Developer,</h1>
+      <h1>A Melbourne based developer,</h1>
       <div class="flex-cols">
         <h2 class="sm-5 md-6 lg-6 h3">Specialising in interface development &amp; design, with a focus on intuitive user experiences.</h2>
       </div>
@@ -17,7 +17,7 @@
       </div>
     </template>
     <template v-slot:plug>
-      <h3 class="plug">I set the gears in motion...</h3>
+      <h3 class="plug">I set the gears in motion.</h3>
     </template>
   </frame-panel>
   <frame-panel v-for="(project, id) in projects"
@@ -53,8 +53,8 @@
       <h2>Who am I?</h2>
       <div class="flex-cols">
         <h3 class="lg-6">I enjoy creating interfaces with web technologies and displaying data in visually appealing ways.<br>
-        When I am not building UI's, I father my young family, enjoy exercising, learning new things &amp; finding inspiration.<br>
-        To read more about my professional history please download my resume <a :href="resumeFile" target="_blank">here</a>.
+        When I am not building UI's, I father my family, enjoy exercising, learning new things &amp; finding inspiration.<br>
+        To read more about my professional history please view my resume <a :href="resumeFile" target="_blank">here</a>.
       </h3>
       </div>
     </template>
@@ -70,8 +70,9 @@
   >
     <template v-slot:article>
       <h2 v-if="sent">Thanks for the message {{form.from_name}}.<br> I will get back to you soon.</h2>
-      <h2 v-else>Please send your enquiries here.</h2>
-      <div v-if="!sent">
+      <template v-else>
+      <h2>Please send your enquiries here.</h2>
+      <div>
         <form class="contact" @submit="onSubmit">
           <div class="flex-cols">
             <div class="lg-9">
@@ -105,8 +106,15 @@
               <div class="flex-cols justify-end">
                 <button type="submit" class="lg-3 button flex align-center justify-center">
                   <template v-if="loading">
-                    <cog class="spin" :styles="{height: '24px', fill: 'transparent',stroke: '#404040', width: '24px'}" />
                     <span class="ml-0-half">sending now</span>
+                    <cog class="spin" :styles="{
+                      position: 'absolute',
+                      right: '18px',
+                      height: '24px',
+                      fill: 'transparent',
+                      stroke: '#fff',
+                      width: '24px',}"
+                    />
                   </template>
                   <span v-else>send message</span>
                 </button>
@@ -115,6 +123,7 @@
           </div>
         </form>
       </div>
+      </template>
     </template>
   </frame-panel>
   <bottom-line /><!-- Contact links -->
@@ -126,7 +135,6 @@ import bottomLine from '../sections/bottomLine.vue'
 import { mapState } from 'vuex'
 import emailjs from 'emailjs-com'
 import cog from '../components/svgs/cogHollow.vue'
-// const resumeFile = require('../../downloads/resume_mark_condello.pdf')
 export default {
   components: { FramePanel, bottomLine, cog },
   data () {
@@ -143,19 +151,17 @@ export default {
     }
   },
   computed: mapState({
-    projects: state => state.projectStore.projects.sort((first, second) => new Date(first.sys.firstPublishedAt) - new Date(second.sys.firstPublishedAt))
+    projects: state => state.projectStore.projects.sort((first, second) => new Date(second.sys.firstPublishedAt) - new Date(first.sys.firstPublishedAt))
   }),
   created () {
     // https://www.emailjs.com/docs/sdk/installation/
     emailjs.init('l-leT1C18Dkw5zcDP')
-    // emailjs.init('user_J6qTVcfRCwXF74Vnqs6JE')
   },
   methods: {
     onSubmit (e) {
       e.preventDefault()
       this.loading = true
       emailjs
-        // .send('service_4c6e8rb', 'template_65qcolf', {
         .send('service_q7zsc1g', 'template_xuyv00f', {
           from_name: this.form.from_name,
           reply_to: this.form.reply_to,
--- todo.md
+++ todo.md
@@ -1,4 +1,4 @@
 
-- [] include new projects created a SGY to folio
+- [] include new projects created at SGY to folio
 - [] flesh out new blog area
 - [x] convert the ProjectService to use Apollo. View the article and code example (here)[https://www.contentful.com/developers/docs/javascript/tutorials/using-contentful-graphql-with-javascript/#:~:text=Apollo%20Client%20is%20one%20of,app%20is%20built%20on%20React.]
